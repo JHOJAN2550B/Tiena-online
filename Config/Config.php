f50ab84e6ac332457aa303fff88f54c8892c98ea
felix <felix1@gmail.com>
2023-11-23 09:18:50 -0600

    proyecto

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/1ING%20HENRY/Tienda-online/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda-youtube";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "AZQX-KB8-6VC3L1q-eJ8QUnfQhTDzBSCOfAHKKC2t811JvYQLZ8zjB5IPqgZnvZzyH_MkzibWFP5AI5P";
+
+const USER_SMTP = "jhojanwmamani@iestpacora.edu.pe";
+const PASS_SMTP = "thjv ohcd inkh vqpy";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
